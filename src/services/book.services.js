6b85ae9c9647832063be3ce9c2e4f703585fd1ab
bhargesh44 <bhargeshgediya44@gmail.com>
2022-03-26 09:31:24 +0530

    first commit

---
+++ src/services/book.services.js
@@ -0,0 +1,39 @@
+import {
+  collection,
+  getDocs,
+  getDoc,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+import { db } from "../firebase";
+
+const bookCollectionRef = collection(db, "books");
+
+class BookDataServices {
+  addBook = (newBook) => {
+    return addDoc(bookCollectionRef, newBook);
+  };
+
+  updateBook = (id, updatedBook) => {
+    const bookDoc = doc(db, "books", id);
+    return updateDoc(bookDoc, updatedBook);
+  };
+
+  deleteBook = (id) => {
+    const bookDoc = doc(db, "books", id);
+    return deleteDoc(bookDoc);
+  };
+
+  getAllBooks = () => {
+    return getDocs(bookCollectionRef);
+  };
+
+  getBook = (id) => {
+    const bookDoc = doc(db, "books", id);
+    return getDoc(bookDoc);
+  };
+}
+
+export default new BookDataServices();
